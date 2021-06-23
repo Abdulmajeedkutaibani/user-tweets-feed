c0ba63e97096f01dc2aea6ff1465c6e66f6b9742
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-06-24 01:59:33 +0300

    make twitter user feed using react and add style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import twitterPhoto from './components/twitter.jpeg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const getAllPosts = async () => {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    setData(response.data);
+  };
+  useEffect(() => {
+    getAllPosts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='tweets-section'>
+        <div className='tweets-header'>
+          <h2>Tweets</h2>
+          <div>
+            <a
+              target='_blank'
+              class='twitter-btn'
+              title='Follow @DaveAlger on Twitter'
+              href='https://twitter.com/intent/follow?screen_name=DaveAlger'
+            >
+              <i></i>
+              <span class='label'>@ThisTwitterUser</span>
+            </a>
+          </div>
+        </div>
+        {data.length > 0
+          ? data.map((item) => (
+              <div key={item.id} className='tweets'>
+                <div className='userPhoto'>
+                  <img src={twitterPhoto} alt='' />
+                </div>
+                <div className='tweet'>
+                  <div className='tweet-info'>
+                    <div className='user'>
+                      <h4>{item.name}</h4>
+                      <span>@{item.username}</span>
+                    </div>
+                    <div className='date'>
+                      <span>24 July</span>
+                    </div>
+                  </div>
+                  <div className='tweet-content'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Officiis, fuga aperiam. Numquam debitis mollitia molestiae.
+                  </div>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 }
